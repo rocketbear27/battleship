f8a36cd12bb221bc21b0377d9a9637dd46fd5359
trea1541 <trea2160@gmail.com>
2024-05-30 21:04:28 -0700

    did soe stuff with AI

--- AI.java
+++ AI.java
@@ -1,68 +1,56 @@
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class AI {
     private Random random = new Random();
-    private int lastHitRow = -1;
-    private int lastHitCol = -1;
-    private boolean lastHitSuccessful = false;
+    private boolean lastHit = false;
     private ArrayList<int[]> coordinates = new ArrayList<int[]>();
 
     /*
-     * if lastHit is false and coord is empty
+     * coordinates is an array list of int arrays
+     * if (lastHit is false and coord is empty)
      * {
      * random gen
      * if (hit)
      * {
      * lastHit = true
      * coordinates.add(all coordinates around it) //make new method that checks if
-     * coord is valid
+     * coord valid
      * }
      * }
-     * else {
-     * if (lastHit is true and coord is not emty)
-     * {
-     * pick a random coordinate from coordinates to attack
-     * if (hit)
+     * else if (coord is not empty) {
+     * guess at one of coords in coordinates
+     * if (hit and ship is not sunk) {
+     * make new coords based on hit (go up or down if ship is vertical or left or
+     * right
+     * if ship is horiz)
      * }
-     * else if (last hit is false and coord is not empty)
-     * {
-     * if (hit)
-     * {
-     * lastHit = true
-     * coordinates.add(all coordinates around it)
+     * if (hit and ship is sunk) {
+     * lastHit = false
+     * empty coordinates
      * }
+     * if (miss) {
+     * delete that from coordinate
      * }
      * }
      */
     public boolean playAI(Player aiPlayer, Player humanPlayer) {
-        boolean aiTurn = true;
         boolean continuePlaying = true;
-
         Battleship game = new Battleship();
 
         while (continuePlaying) {
-            if (aiTurn) {
-                if (!attackHard(aiPlayer, humanPlayer)) {
-                    // AI missed, switch turns
-                    aiTurn = false;
-                }
-            } else {
-                if (!game.attack(humanPlayer, aiPlayer)) {
-                    // Human missed, switch turns
-                    aiTurn = true;
-                }
+            if (game.ifPlayerHasWon(aiPlayer)) {
+                return false; // Human player has won
             }
-
+            if (!attackHard(aiPlayer, humanPlayer)) {
+                continuePlaying = false;
+            }
             if (game.ifPlayerHasWon(humanPlayer)) {
-                return false; // AI has won, stop the game
+                return false;
             }
-
-            continuePlaying = !game.ifPlayerHasWon(humanPlayer); // Check if game should continue
         }
 
-        return true; // Game is still ongoing
+        return true;
     }
 
     public void placeShipsEasy(Player aiPlayer) {
@@ -97,19 +85,85 @@ public class AI {
                 String[] orientations = { "H", "V" };
                 String orientation = orientations[(int) (Math.random() * orientations.length)];
                 if (Battleship.checkCoordinateValidity(row, col, length, orientation, aiPlayer.getGameBoard())) {
-                    if (!touchingOtherShips(row, col, length, orientation, aiPlayer.getGameBoard())) {
-                        Battleship.placeShipOnBoard(row, col, length, orientation, aiPlayer.getGameBoard());
-                        aiPlayer.getShips().add(new Ship(row, col, length, orientation));
-                        placed = true;
-                    }
+                    Battleship.placeShipOnBoard(row, col, length, orientation, aiPlayer.getGameBoard());
+                    aiPlayer.getShips().add(new Ship(row, col, length, orientation));
+                    placed = true;
                 }
             }
         }
     }
 
     public boolean attackHard(Player aiPlayer, Player humanPlayer) {
-        // implement still
-        return false; // placeholders
+        if (!lastHit && coordinates.isEmpty()) {
+            return huntMode(aiPlayer, humanPlayer);
+        } else {
+            return targetMode(aiPlayer, humanPlayer);
+        }
+    }
+
+    private boolean huntMode(Player aiPlayer, Player humanPlayer) {
+        while (true) {
+            char row = (char) ((int) (Math.random() * 10) + 'A');
+            int col = (int) (Math.random() * 10) + 1;
+            if (Battleship.isValidAttack(row, col, aiPlayer.getAttackBoard())) {
+                boolean hit = Battleship.aiAttack(aiPlayer, humanPlayer, row, col);
+                if (hit) {
+                    addSurroundingCoordinates(row, col, humanPlayer);
+                    lastHit = true;
+                }
+                return hit;
+            }
+        }
+    }
+
+    private boolean targetMode(Player aiPlayer, Player humanPlayer) {
+        while (!coordinates.isEmpty()) {
+            int[] coord = coordinates.remove(0);
+            char row = (char) (coord[0] + 'A');
+            int col = coord[1];
+            if (Battleship.isValidAttack(row, col, aiPlayer.getAttackBoard())) {
+                boolean hit = Battleship.aiAttack(aiPlayer, humanPlayer, row, col);
+                if (hit) {
+                    if (isShipSunk(humanPlayer, row, col)) {
+                        lastHit = false;
+                        coordinates.clear();
+                    } else {
+                        addSurroundingCoordinates(row, col, humanPlayer);
+                    }
+                }
+                return hit;
+            }
+        }
+        lastHit = false;
+        return huntMode(aiPlayer, humanPlayer);
+    }
+
+    private void addSurroundingCoordinates(int row, int col, Player humanPlayer) {
+        int[][] potentialCoordinates;
+
+        if (lastHit) {
+            Ship lastHitShip = humanPlayer.getShips().get(humanPlayer.getShips().size() - 1);
+            if (lastHitShip.getOrientation().equals("H")) {
+                potentialCoordinates = new int[][] { { row, col - 1 }, { row, col + 1 } };
+            } else {
+                potentialCoordinates = new int[][] { { row - 1, col }, { row + 1, col } };
+            }
+        } else {
+            // If lastHit is false, add coordinates around the hit position
+            potentialCoordinates = new int[][] {
+                    { row - 1, col }, { row + 1, col }, { row, col - 1 }, { row, col + 1 }
+            };
+        }
+
+        for (int[] coord : potentialCoordinates) {
+            if (isValidCoordinate(coord[0], coord[1])) {
+                coordinates.add(coord);
+            }
+        }
+    }
+
+    private boolean isValidCoordinate(int row, int col) {
+        return row >= 0 && row < 10 && col >= 0 && col < 10;
     }
 
     private boolean randomAttack(Player aiPlayer, Player humanPlayer) {
@@ -122,45 +176,12 @@ public class AI {
         }
     }
 
-    private int[] getLastHitCoordinates(Player aiPlayer) {
-        for (int row = 0; row < 10; row++) {
-            for (int col = 0; col < 10; col++) {
-                if (aiPlayer.getAttackBoard()[row][col].equals("H")) {
-                    return new int[] { row, col };
-                }
-            }
-        }
-        return null;
-    }
-
-    private boolean touchingOtherShips(char row, int col, int length, String orientation, String[][] gameBoard) {
-        int i = row - 'A' + 1;
-        int j = col;
-        if (orientation.equals("H")) {
-            for (int a = 0; a < length; a++) {
-                if (isAdjacentOccupied(i, j + a, gameBoard)) {
-                    return true;
-                }
-            }
-        } else if (orientation.equals("V")) {
-            for (int a = 0; a < length; a++) {
-                if (isAdjacentOccupied(i + a, j, gameBoard)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private boolean isAdjacentOccupied(int i, int j, String[][] gameBoard) {
-        int[] x = { -1, 0, 1, 0 };
-        int[] y = { 0, 1, 0, -1 };
-        for (int k = 0; k < 4; k++) {
-            int a = i + x[k];
-            int b = j + y[k];
-            if (a > 0 && a < gameBoard.length && b > 0 && b < gameBoard[0].length) {
-                if (gameBoard[a][b].equals("+")) {
-                    return true;
+    public static boolean isShipSunk(Player player, char row, int col) {
+        for (Ship ship : player.getShips()) {
+            ArrayList<int[]> shipCoordinates = ship.getCoordinates();
+            for (int[] coordinate : shipCoordinates) {
+                if (coordinate[0] == row - 'A' + 1 && coordinate[1] == col) {
+                    return ship.isSunk();
                 }
             }
         }
